8c8de53c2318e32f6a104b1c1a2021eea5a8c229
Camilo Castillo <npgcamilo@gmail.com>
2019-12-02 17:13:21 -0500

    03-Actions

--- src/app/customers/state/customer.reducer.ts
+++ src/app/customers/state/customer.reducer.ts
@@ -1,31 +1,64 @@
+import * as customerActions from './customer.actions';
+import { Customer } from "../customer.model";
+import * as fromRoot from "../../state/app-state";
 import { Action } from '@ngrx/store';
 
-const initialState = {
-    customers:[
-        {
-            name : "John Doe",
-            phone : "4654564564",
-            address: "st",
-            membership: "Platinum",
-            id: 1
-        }
-    ],
+
+export interface CustomerState{
+    customers: Customer[],
+    loading: boolean,
+    loaded: boolean,
+    error: string
+}
+
+export interface AppState extends fromRoot.AppState{
+    customers : CustomerState
+}
+
+export const initialState: CustomerState = {
+    customers: [],
     loading: false,
-    loaded: true
+    loaded: false,
+    error: ""
 }
 
 
-export function customerReducer(state = initialState, action:Action){
+export function customerReducer(state = initialState, action: customerActions.Action): CustomerState{
     switch (action.type) {
-        case "LOAD_CUSTOMER":{
+        case customerActions.CustomerActionTypes.LOAD_CUSTOMERS:{
+            /**
+             * El siguiente codigo es igual a 
+             *  
+             *      state.loading = true;
+             *      return state:
+             */
             return {
+            ...state,
+            loading: true
+            } 
+        }
+
+        case customerActions.CustomerActionTypes.LOAD_CUSTOMERS_SUCCESS:{
+            return{
                 ...state,
-                loading: true,
-                loaded: false
+                loading: false,
+                loaded: true,
+                customers: action.payload
             }
         }
-                                    
-        default:
+
+        case customerActions.CustomerActionTypes.LOAD_CUSTOMERS_FAIL:{
+            return{
+                ...state,
+                customers: [],
+                loading: false,
+                loaded: false,
+                error: action.payload
+            }
+        }
+    
+        default:{
             return state;
+        }
     }
 }
